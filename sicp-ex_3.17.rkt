dfac95cfa3c36382d4a204e13c003dfd598e3122
Max Beutel <me@maxbeutel.de>
2020-04-19 13:55:17 +0200

    3.17

---
+++ sicp-ex_3.17.rkt
@@ -0,0 +1,45 @@
+#lang sicp
+
+(define (contains x L)
+  (cond ((null? L) #f)
+        ((eq? (car L) x) #t)
+        (else (contains x (cdr L)))))
+
+(define (count-pairs x)
+  (let ((encountered '()))
+    (define (helper x)
+      (cond ((or (not (pair? x)) (memq x encountered)) 0)
+            (else (begin
+                    (set! encountered (cons x encountered))
+                    (+ (helper (car x))
+                       (helper (cdr x))
+                       1)))))
+    (helper x)))
+
+;;
+(define u '(a b c))
+
+(count-pairs u)
+
+;;
+(define v1 '(a))
+(define v2 '(b))
+(define v (cons v1 v2))
+(set-cdr! v1 v2)
+
+(count-pairs v)
+
+;;
+(define w1 '(a))
+(define w2  (cons w1 w1))
+(define w (cons w2 w2))
+
+(count-pairs w)
+
+;;
+(define x1 '(b))
+(define x2 '(a))
+(define x (cons x1 x2))
+(set-cdr! x2 x)
+
+(count-pairs x)
